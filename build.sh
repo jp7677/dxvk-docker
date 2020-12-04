4cb23338ff287ccf16d39cee59f4b71a6cc72a6c
Jens Peters <jp7677@gmail.com>
2020-12-04 10:43:15 +0100

    Remove no longer needed step.

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ if ! [ -z "$1" ]; then
     target=$1
 fi
 
-mkdir -p /root/build/out
 rm -Rf /root/build/out/$target-git
 /root/build/package-release.sh git /root/build/out --no-package
 chown -R $UID:$GID /root/build/out/$target-git
