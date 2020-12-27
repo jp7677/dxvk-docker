1d6bf5b1350b36890a266726dbab77461ecd24ce
Jens Peters <jp7677@gmail.com>
2020-12-27 13:03:34 +0100

    Fix paths.

--- install-dxvk.sh
+++ install-dxvk.sh
@@ -13,16 +13,16 @@ rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d10
 rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d10_1.dll
 rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/d3d9.dll
 
-cp --verbose out/dxvk-master/x64/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
-cp --verbose out/dxvk-master/x64/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
-cp --verbose out/dxvk-master/x64/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
-cp --verbose out/dxvk-master/x64/d3d10.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
-cp --verbose out/dxvk-master/x64/d3d10_1.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
-cp --verbose out/dxvk-master/x64/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/d3d10.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/d3d10_1.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
+cp --verbose out/dxvk-git/x64/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/dxvk/
 
-cp --verbose out/dxvk-master/x32/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
-cp --verbose out/dxvk-master/x32/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
-cp --verbose out/dxvk-master/x32/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
-cp --verbose out/dxvk-master/x32/d3d10.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
-cp --verbose out/dxvk-master/x32/d3d10_1.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
-cp --verbose out/dxvk-master/x32/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/d3d10.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/d3d10_1.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
+cp --verbose out/dxvk-git/x32/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/dxvk/
