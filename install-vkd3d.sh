ed90a1634cebf7135b9235c85cdb25f990a58431
Jens Peters <jp7677@gmail.com>
2021-02-19 21:00:13 +0100

    Avoid prompt.

--- install-vkd3d.sh
+++ install-vkd3d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/d2d12.dll
-rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/d2d12.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/d3d12.dll
+rm --force ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/d3d12.dll
 
 cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/
 cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/
