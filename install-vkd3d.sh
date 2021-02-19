10d05e5d34846319553934ad1d63bb5acb5731ea
Jens Peters <jp7677@gmail.com>
2021-02-19 20:58:52 +0100

    Fix typo.

--- install-vkd3d.sh
+++ install-vkd3d.sh
@@ -3,5 +3,5 @@
 rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/d2d12.dll
 rm ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/d2d12.dll
 
-cp --verbose out/vkd3d-proton-git/x64/d2d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/
-cp --verbose out/vkd3d-proton-git/x86/d2d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/
+cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib64/wine/vkd3d-proton/
+cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 5.13/dist/lib/wine/vkd3d-proton/
