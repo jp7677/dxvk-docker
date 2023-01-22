45ccefba9ede04ad530d01bbc9607d6b6e3cb99a
Jens Peters <jp7677@gmail.com>
2023-01-22 19:03:44 +0100

    Add argument for test filters

--- test-dxvk-nvapi.sh
+++ test-dxvk-nvapi.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+FILTER="$1"
+if [ -z "$1" ]; then
+  FILTER="[@all]"
+fi
+
 DXVK_LOG_LEVEL=none \
 DXVK_NVAPI_LOG_LEVEL=none \
 WINEDEBUG=-all \
 WINEDLLOVERRIDES=nvapi64=n \
 WINEDLLPATH=$(realpath ../../Wine/nvidia-libs/) \
 WINEPATH=$(realpath ../../Wine/nvidia-libs/) \
-wine out/dxvk-nvapi-git/x64/nvapi64-tests.exe [@all]
+wine out/dxvk-nvapi-git/x64/nvapi64-tests.exe "$FILTER"
