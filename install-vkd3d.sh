14397a6788f8a1e2da22e720ffd2c2f69fd7f9a0
Jens Peters <jp7677@gmail.com>
2023-01-28 17:19:51 +0100

    Install into Proton - Experimental
    When available.

--- install-vkd3d.sh
+++ install-vkd3d.sh
@@ -5,3 +5,11 @@ rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proto
 
 cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/vkd3d-proton/
 cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proton/
+
+if [ -e ~/.steam/steam/steamapps/common/Proton\ -\ Experimental ]; then
+    rm --force ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/vkd3d-proton/d3d12.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/d3d12.dll
+
+    cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib64/wine/vkd3d-proton/
+    cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ -\ Experimental/files/lib/wine/vkd3d-proton/
+fi
