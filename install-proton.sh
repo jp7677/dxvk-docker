8a79f4f1900bf5f9d6ab13120b5e3597ffb0a54d
Jens Peters <jp7677@gmail.com>
2023-04-17 21:21:27 +0200

    Update for Proton 8.0

--- install-proton.sh
+++ install-proton.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
 if [ -e out/dxvk-git ]; then
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/dxgi.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/d3d11.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/d3d10core.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/d3d9.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/dxgi.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/d3d11.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/d3d10core.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/d3d9.dll
 
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/dxgi.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/d3d11.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/d3d10core.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/d3d9.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/dxgi.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/d3d11.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/d3d10core.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/d3d9.dll
 
-    cp --verbose out/dxvk-git/x64/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/
-    cp --verbose out/dxvk-git/x64/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/
-    cp --verbose out/dxvk-git/x64/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/
-    cp --verbose out/dxvk-git/x64/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/dxvk/
+    cp --verbose out/dxvk-git/x64/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/
+    cp --verbose out/dxvk-git/x64/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/
+    cp --verbose out/dxvk-git/x64/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/
+    cp --verbose out/dxvk-git/x64/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/dxvk/
 
-    cp --verbose out/dxvk-git/x32/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/
-    cp --verbose out/dxvk-git/x32/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/
-    cp --verbose out/dxvk-git/x32/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/
-    cp --verbose out/dxvk-git/x32/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/dxvk/
+    cp --verbose out/dxvk-git/x32/dxgi.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/
+    cp --verbose out/dxvk-git/x32/d3d11.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/
+    cp --verbose out/dxvk-git/x32/d3d10core.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/
+    cp --verbose out/dxvk-git/x32/d3d9.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/dxvk/
 fi
 
 if [ -e out/dxvk-nvapi-git ]; then
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/nvapi/nvapi64.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/nvapi/nvapi.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/nvapi/nvapi64.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/nvapi/nvapi.dll
 
-    cp --verbose out/dxvk-nvapi-git/x64/nvapi64.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/nvapi/
-    cp --verbose out/dxvk-nvapi-git/x32/nvapi.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/nvapi/
+    cp --verbose out/dxvk-nvapi-git/x64/nvapi64.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/nvapi/
+    cp --verbose out/dxvk-nvapi-git/x32/nvapi.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/nvapi/
 fi
 
 if [ -e out/vkd3d-proton-git ]; then
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/vkd3d-proton/d3d12core.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/vkd3d-proton/d3d12.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proton/d3d12core.dll
-    rm --force ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proton/d3d12.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/vkd3d-proton/d3d12core.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/vkd3d-proton/d3d12.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/vkd3d-proton/d3d12core.dll
+    rm --force ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/vkd3d-proton/d3d12.dll
 
-    cp --verbose out/vkd3d-proton-git/x64/d3d12core.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/vkd3d-proton/
-    cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib64/wine/vkd3d-proton/
-    cp --verbose out/vkd3d-proton-git/x86/d3d12core.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proton/
-    cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 7.0/dist/lib/wine/vkd3d-proton/
+    cp --verbose out/vkd3d-proton-git/x64/d3d12core.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/vkd3d-proton/
+    cp --verbose out/vkd3d-proton-git/x64/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib64/wine/vkd3d-proton/
+    cp --verbose out/vkd3d-proton-git/x86/d3d12core.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/vkd3d-proton/
+    cp --verbose out/vkd3d-proton-git/x86/d3d12.dll ~/.steam/steam/steamapps/common/Proton\ 8.0/dist/lib/wine/vkd3d-proton/
 fi
 
 if [ -e ~/.steam/steam/steamapps/common/Proton\ -\ Experimental ]; then
