ca9bb9f5554d7114e3b48e0acab63afcbbe4fcdf
Jens Peters <jp7677@gmail.com>
2025-02-02 13:51:11 +0100

    Run nvofapi tests

--- test-dxvk-nvapi.sh
+++ test-dxvk-nvapi.sh
@@ -14,3 +14,8 @@ WINEDLLOVERRIDES=nvapi64=n \
 WINEDLLPATH=$(realpath ../../Wine/nvidia-libs/) \
 WINEPATH=$(realpath ../../Wine/nvidia-libs/) \
 wine out/dxvk-nvapi-git/x64/nvapi64-tests.exe "$FILTER"
+
+DXVK_NVAPI_LOG_LEVEL=none \
+WINEDEBUG=-all \
+WINEDLLOVERRIDES=nvofapi64=n \
+wine out/dxvk-nvapi-git/x64/nvofapi64-tests.exe "$FILTER"
