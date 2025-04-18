d864ba196554ef9398b8beaf55ae0dbfa1ca927b
Jens Peters <jp7677@gmail.com>
2025-04-18 11:42:31 +0200

    Set NGX registry keys

--- test-dxvk-nvapi.sh
+++ test-dxvk-nvapi.sh
@@ -9,6 +9,7 @@ DXVK_LOG_LEVEL=none \
 DXVK_HDR=1 \
 DXVK_NVAPI_LOG_LEVEL=none \
 DXVK_NVAPI_ALLOW_OTHER_DRIVERS=1 \
+DXVK_NVAPI_SET_NGX_DEBUG_OPTIONS=DLSSIndicator=0,DLSSGIndicator=0,Logging=0,LogLevel=0 \
 WINEDEBUG=-all \
 WINEDLLOVERRIDES=nvapi64=n \
 WINEDLLPATH=$(realpath ../../Wine/nvidia-libs/) \
